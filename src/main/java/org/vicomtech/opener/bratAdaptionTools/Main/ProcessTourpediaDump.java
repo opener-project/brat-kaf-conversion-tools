9fec98c4941d79f51d7ac34bff3e2bf5a4bd382d
Aitor Garcia <agarciap@vicomtech.org>
2013-11-18 10:08:02 +0100

    First version of tourpedia dump process

---
+++ src/main/java/org/vicomtech/opener/bratAdaptionTools/Main/ProcessTourpediaDump.java
@@ -0,0 +1,65 @@
+package org.vicomtech.opener.bratAdaptionTools.Main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+public class ProcessTourpediaDump {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		InputStream is;
+		if(args.length==0){
+			URL url=new URL("http://wafi.iit.cnr.it/openervm/API/getBratForVenues.php");
+			is=url.openStream();
+			processTourpediaDump(is, System.out);
+		}else{
+			System.err.println("Not implemented");
+		}
+
+	}
+	
+	public static void processTourpediaDump(InputStream is, OutputStream os){
+		try {
+			List<String>entries=IOUtils.readLines(is);
+			for(String entry:entries){
+				String processedEntry=processEntry(entry);
+				IOUtils.write(processedEntry.getBytes(), os);
+				IOUtils.write("\n".getBytes(), os);
+			}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	protected static String processEntry(String entry){
+		String[]columns=entry.split("\t");
+		String id=columns[0];
+		String nameCol=columns[1];
+		String descriptionCol=columns[2];
+		String urlCol=columns[3];
+		if(urlCol.trim().endsWith(":")){
+			urlCol=urlCol.trim()+"n/a";
+		}
+		StringBuffer sb=new StringBuffer();
+		sb.append(id);
+		sb.append("\t");
+		sb.append(nameCol.replace("name:", "name:Name:"));
+		sb.append("\t");
+		sb.append(descriptionCol.replace("attr:", "attr:Description:"));
+		sb.append("\t");
+		sb.append(urlCol.replace("info:", "attr:Web page:"));
+		return sb.toString();
+	}
+
+}
